c6bcc676ea0ae6d0488e81f5afd96babae7f6518
GingShan <105120159+GingShan@users.noreply.github.com>
2023-08-21 16:17:00 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(function(){
+    $.ajax({
+        method: "GET",
+        url: "https://gingshan.github.io/jsonTest/data/data.json",
+        dataType: "json",
+        data: {},
+    }).done(function(res){
+        $('#users').empty()
+        res.forEach(function(user){
+            $('#users').append('<li>' + user.id + ". " + user.name + '</li>');
+        })
+    }).fail(function(err){
+        console.log(err)
+    })
+
+    $('#create a').on('click',function(){
+        let name = $('#create > input').val().trim()
+        if (!name) return false
+        $.ajax({
+            method: "post",
+            url: "https://gingshan.github.io/jsonTest/data/data.json",
+            dataType: "json",
+            data: {
+                name: name
+            }
+        });
+    })
+
+    $('#delete a').on('click',function(){
+        let id = $('#delete > input').val().trim()
+        if (!id) return false
+        $.ajax({
+            method: "delete",
+            url: "https://gingshan.github.io/jsonTest/data/data.json/" + id,
+            dataType: "json",
+
+        }).done(function(res){
+            console.log(res)
+            $('#users').empty()
+            res.forEach(function(user){
+                console.log(user)
+            });
+        })
+        .fail(function(err){
+            console.log(err)
+        })
+    })
+    $('#update a').on('click',function(){
+        let id = $('#update > input[name=id]').val().trim()
+        if (!id) return false
+        let name = $('#update > input[name=name]').val().trim()
+        if (!name) return false
+        $.ajax({
+            method: "put",
+            url: "https://gingshan.github.io/jsonTest/data/data.json/" + id,
+            dataType: "json",
+            data:{
+                name: name
+            }
+        });
+    })
+})
